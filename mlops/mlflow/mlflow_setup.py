b888fb90d957c979f7e5ffe6071ab3650be606b2
Vamshi Krishna S <107762231+VamshiKrsna@users.noreply.github.com>
2024-09-09 20:37:28 +0530

    restart

--- mlops/mlflow/mlflow_setup.py
+++
@@ -1,9 +0,0 @@
-import mlflow
-import mlflow.keras
-from lstm import train_lstm_model
-
-if __name__ == "__main__":
-    mlflow.set_experiment("stockr_lstm")
-    with mlflow.start_run():
-        model = train_lstm_model()
-        mlflow.keras.log_model(model, "model")
